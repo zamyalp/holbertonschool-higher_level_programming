f098519f0fe7c7e0049e16688616ab54e1667945
zamyalp <zamyalp03@gmail.com>
2022-08-31 16:06:57 -0700

    3

---
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+safe_print_division = __import__('3-safe_print_division').safe_print_division
+a = 12
+b = 2
+div=num1/num2;
+print("The division of {0} and {1} is: {2}".format(num1,num2,div))
