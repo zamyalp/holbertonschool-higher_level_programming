a4031a9fe82cb0668eabf2bd0d044525d78ddfd0
zamyalp <zamyalp03@gmail.com>
2022-08-31 15:26:02 -0700

    1

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    if argv_count is 0:
+import sys
+argv = sys.argv[1:]
+argv_count = len(argv)
+index = 1
+if argv_count is 0:
     print("{:d} arguments.".format(argv_count))
 elif argv_count is 1:
     print("{:d} argument:".format(argv_count))
