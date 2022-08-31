bb0f7df04996cd7fe7b441425cf372aa9509961b
zamyalp <zamyalp03@gmail.com>
2022-08-31 13:46:31 -0700

    7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-
 def islower(c):
-        if ord(c) >= 97 and ord(c) <= 122:
-                    return True
-                    return False
-
-
-                if __name__ == '__main__':
-                        print(islower('C'))
+            if ord(c) >= ord('a') and ord(c) <= ord('z'):
+                            return True
+            else:
+                            return False
