96fb91373c2e91bc86e936565583d4d5248c1242
zamyalp <zamyalp03@gmail.com>
2022-08-31 13:23:29 -0700

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-        print("{:c}".format(c), end="")
+for a in range(97, 123):
+            print('{:c}'.format(a), end='')
+            
