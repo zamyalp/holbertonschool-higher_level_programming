4a4b9f55062fed820b2953667c372c20f0ad63df
zamyalp <zamyalp03@gmail.com>
2022-08-31 16:41:45 -0700

    3

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div=a/b
+        div = a/b
     except ZeroDivisionError:
-        div="none"
+        div = "none"
     finally:
-        print("The division of {0} and {1} is: {2}".format(a,b,div))
-        return div 
+        print("Inside result: {0} and {1} is: {2}".format(a,b,div))
+        return div
