2937bc746242b63321335a79b28bc5a625683ea3
zamyalp <zamyalp03@gmail.com>
2022-08-31 18:21:06 -0700

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_low_alpha():
-                        for c in 'abcdfghijklmnoprstuvwxyz':
-                                                    print('{}'.format(c), end='')
-
-                                                    if __name__ == '__main__':
-                                                                            print_low_alpha()
+   for c in 'abcdfghijklmnoprstuvwxyz':
+        print('{}'.format(c), end='')
