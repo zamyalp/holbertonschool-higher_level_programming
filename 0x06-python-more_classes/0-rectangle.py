5c3173a15d55ca37b3b5590407917e3111ac59f4
zamyalp <zamyalp03@gmail.com>
2022-09-13 18:47:30 -0700

    rectangle

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Module 0-rectangle
-Defines an empty Rectangle class.
+"""
+A rectangle.
 """
 
 
 class Rectangle:
-    """Empty class."""
+    """
+    Rectangle functions and data
+    """
+    
     pass
