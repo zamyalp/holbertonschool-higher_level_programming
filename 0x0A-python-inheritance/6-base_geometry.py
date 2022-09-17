28681a5da92575bb5109bb44ef4023c1b31dfc7a
zamyalp <zamyalp03@gmail.com>
2022-09-16 21:32:18 -0700

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+   Module 6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """New class"""
+    def area(self):
+        """BaseGeometry"""
+        raise Exception("area() is not implemented")
