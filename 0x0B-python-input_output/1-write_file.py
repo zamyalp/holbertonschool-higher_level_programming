63987c3b2a3695dde08ef28a056ef953dc28bbe9
zamyalp <zamyalp03@gmail.com>
2022-09-19 18:35:35 -0700

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 
 def write_file(filename="", text=""):
     """counts lines in filename."""
-    with open(filename, mode='w', encoding="UTF8") as f:
+    with open(filename, 'w', encoding="UTF8") as f:
         return f.write(text)
