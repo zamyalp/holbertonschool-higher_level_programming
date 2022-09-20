c92501661add130cef2a662072aac887f3fe2307
zamyalp <zamyalp03@gmail.com>
2022-09-19 18:04:43 -0700

    stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-    Module 0-read_file.py
-"""
+""" Module 0-read_file.py """
 
 
 def read_file(filename=""):
-    """reads from file and print to stdout"""
-    with open(filename, 'r', encoding="UTF8") as f:
-        print(f.read(), end='')
+    """Reads from filename and prints its contents to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
