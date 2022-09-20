878f06ee9d244cb206a0037d56d2e8a63b4773ea
zamyalp <zamyalp03@gmail.com>
2022-09-19 18:45:01 -0700

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,6 @@
 """ Module 1-write_file """
 
 
-
 def write_file(filename="", text=""):
     """Will write to a text file and return the number of characters"""
     with open(filename, 'w', encoding="UTF8") as f:
