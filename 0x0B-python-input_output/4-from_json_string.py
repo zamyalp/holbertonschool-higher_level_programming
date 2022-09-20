8dddc11c1787f6e39966981a2ee95ced8044af7a
zamyalp <zamyalp03@gmail.com>
2022-09-19 19:18:36 -0700

    json string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module 4-from_json_string """
+import json
+
+
+def from_json_string(my_str):
+    """ returns an object represented by JSON string.
+    """
+        return json.loads(my_str)
