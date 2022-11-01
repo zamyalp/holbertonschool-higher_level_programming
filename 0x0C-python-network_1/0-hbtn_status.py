192488a44e15358892ebff81da72d1958fab5e08
zamyalp <zamyalp03@gmail.com>
2022-11-01 15:44:53 -0700

    1

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -6,9 +6,8 @@ from urllib import request
 
 url = 'https://intranet.hbtn.io/status'
 with request.urlopen(url) as response:
-    the_page = response.read()
+    response = response.read()
     print('Body response:')
     print('\t- type: {}'.format(type(the_page)))
     print('\t- content: {}'.format(the_page))
     print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
-    sep='\n\t')
