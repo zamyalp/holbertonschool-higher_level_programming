87ded699922ab597bd3ba0c52237d8e8cc3e3ec5
zamya <zamyalp03@gmail.com>
2022-12-15 01:37:08 +0000

    hello

---
+++ 0x03-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+$.ajax({
+  url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+  type: 'GET',
+  dataType: 'json'
+})
+  .done(function (json) {
+    $('DIV#hello').text(json.hello);
+  });
